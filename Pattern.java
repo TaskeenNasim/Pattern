e2fe8dfe0ac06178dfa399f1b3ebd2c70f37b15d
TaskeenNasim <99030290+TaskeenNasim@users.noreply.github.com>
2022-06-03 01:34:31 +0530

    make peramids pattern

---
+++ Pattern.java
@@ -0,0 +1,27 @@
+public class patterns {
+    public static void main(String args[]){
+        int n = 4;
+        int m = 5;
+        // outer loop
+        for(int i=1; i<=n; i++){
+            
+            // inner loop
+            for(int j=1; j<=m; j++){
+
+            // {
+            //     System.out.print("*");
+            //     }
+            //     System.out.println();
+
+            // cell ->(i,j)
+            if(i == 1 || j == 1 || i == n || j == m) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println();    
+    }
+}
+    
+}
